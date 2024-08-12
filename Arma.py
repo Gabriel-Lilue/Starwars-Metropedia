aef0574015a7b5e111cebc39d2410d9d1e468b4e
santiago larrazabal <santiago.larrazabal@correo.unimet.edu.ve>
2024-08-11 20:23:29 -0400

    No se montaron las clases de Mision y Arma

--- Arma.py
+++ Arma.py
@@ -1,4 +1,19 @@
-# Para el segundo pedazo
-
 class Arma:
-    pass
+    def __init__(self, id,name,model,manufacturer,cost_in_credits ,length ,weapon_type,description,films):
+        self.id = id
+        self.name = name
+        self.model = model
+        self.manufacturer = manufacturer
+        self.cost_in_credits = cost_in_credits
+        self.length = length
+        self.weapon_type = weapon_type
+        self.description = description
+        self.films = films
+
+    def show(self):
+        return f'''ID {self.id}. --------
+NOMBRE: {self.name} - MODELO: {self.model} - FABRICANTE: {self.manufacturer}
+TIPO: {self.weapon_type} - COSTO EN CREDITOS: {self.cost_in_credits} - LONGITUD: {self.length}
+DESCRIPCION: {self.description}
+PELICULAS: {self.films}
+'''
--- Mision.py
+++ Mision.py
@@ -1,2 +1,51 @@
 class Mision:
-    pass
+    def __init__(self, id,name, destination, spaceship, weapons, characters):
+        self.id = id
+        self.name = name
+        self.destination = destination
+        self.spaceship = spaceship
+        self.weapons = weapons
+        self.characters = characters
+
+    def show(self):
+        return f'''{self.id} - INFORMACIÓN DE LA MISIÓN - 
+NOMBRE: {self.name}
+PLANETA DESTINO: {self.destination.name}
+ARMAS:
+{self.show_array(self.weapons)}
+INTEGRANTES:
+{self.show_array(self.characters)}
+'''
+    
+    def show_array(self,lista):
+        msg = ''
+        if len(lista) == 0:
+            return 'No especificado'
+        else:
+            for i,element in enumerate(lista):
+                if i != len(lista)-1:
+                    msg += f'{element.name}, '
+                else:
+                    msg += f'{element.name}'
+            return msg
+    
+    def show_array2(self,lista):
+        pass
+
+    def show_destination(self):
+        print('DESTINO\n')
+        print(self.destination.show())
+    
+    def show_spaceship(self):
+        print('NAVE ESPACIAL\n')
+        print(self.spaceship.show())
+    
+    def show_weapons(self):
+        print('ARMAS\n')
+        for weapon in self.weapons:
+            print(weapon.show())
+
+    def show_characters(self):
+        print('INTEGRANTES\n')
+        for character in self.characters:
+            print(character.show())
